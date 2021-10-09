07327ed5a55be62e0339010b29f0257519e5104d
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-09 14:35:28 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/Image-Editor-Bot.git /Image-Editor-Bot
+cd /Image-Editor-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
