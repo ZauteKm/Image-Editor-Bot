22ed36e81d69bbe89254b2786a06cb4c50e7e95c
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-10-09 13:49:57 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+class script(object):
+
+    START_MSG = """
+<b>Hello {} , I'am a simple Image Editor Bot which Supports various modes.</b>
+"""
+    HELP_MSG = """
+<b><u>Help and Informations</u></b>
+
+▷ Send me any Image to Edit
+▷ Select the Corresponding mode that you need
+▷ Your Edited Image will be Uploaded
+"""
+    BANNED_USER_TEXT = "<code>You are Banned!</code>"
+    SOMETHING_WRONG = "Something wrong. Contact @ZauteBot."
+    FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+    FREE_USER_LIMIT = "Sorry Friend, Free users can only 1 request per {} minutes. Please try again after {} seconds later."
